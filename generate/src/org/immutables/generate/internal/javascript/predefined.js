dfac6a3305eba5e083c157d91ea6ed624114191d
Eugene Lukash <e.lucash@gmail.com>
2014-10-18 03:02:33 -0700

    removed old generate module, superseded by generator and individual annotation processors

--- generate/src/org/immutables/generate/internal/javascript/predefined.js
+++
@@ -1,5 +0,0 @@
-{
-  String.prototype.toUpperFirst = function () this.charAt(0).toUpperCase() + this.substring(1)
-
-  String.prototype.toLowerFirst = function () this.charAt(0).toLowerCase() + this.substring(1)
-}
