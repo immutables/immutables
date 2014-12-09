c64b46d9b97efbc05d38d50780032b909f901350
IVysharev <ivysharev@gmail.com>
2014-12-09 23:39:04 +0200

    start working on finite state postprocessing machine

---
+++ generator/src/org/immutables/generator/FiniteStatePostprocessingMachine.java
@@ -0,0 +1,12 @@
+package org.immutables.generator;
+
+
+final class FiniteStatePostprocessingMachine {
+  private FiniteStatePostprocessingMachine() {
+  }
+
+  static CharSequence rewrite(CharSequence content) {
+    // TODO
+    return "";
+  }
+}
---
+++ generator/test/org/immutables/generator/FiniteStatePostprocessingMachineTest.java
@@ -0,0 +1,78 @@
+
+package org.immutables.generator;
+
+import com.google.common.base.Joiner;
+import org.junit.Ignore;
+import org.junit.Test;
+import static org.immutables.check.Checkers.*;
+
+public class FiniteStatePostprocessingMachineTest {
+  private static final Joiner LINES = Joiner.on('\n');
+
+  @Test
+  @Ignore
+  public void imports() {
+    CharSequence rewrited = FiniteStatePostprocessingMachine.rewrite(
+        LINES.join("package start;",
+            "import java.util.List;",
+            "class My extends java.util.Set {}"));
+
+    check(rewrited).hasToString(
+        LINES.join("package start;",
+            "import java.util.Set;",
+            "import java.util.List;",
+            "class My extends Set {}"));
+  }
+
+  @Test
+  @Ignore
+  public void generatedImportsPlaceholder() {
+    CharSequence rewrited = FiniteStatePostprocessingMachine.rewrite(
+        LINES.join("package start;",
+            "import java.util.List;",
+            "// Generated imports",
+            "",
+            "class My extends java.util.Set {}"));
+
+    check(rewrited).hasToString(
+        LINES.join("package start;",
+            "import java.util.List;",
+            "import java.util.Set;",
+            "",
+            "class My extends Set {}"));
+  }
+
+  @Test
+  @Ignore
+  public void javaLangImports() {
+    CharSequence rewrited = FiniteStatePostprocessingMachine.rewrite(
+        LINES.join("package start;",
+            "class My extends java.lang.Throwable {}"));
+
+    check(rewrited).hasToString(
+        LINES.join("package start;",
+            "class My extends Throwable {}"));
+  }
+
+  @Test
+  @Ignore
+  public void importsNoPlaceholders() {
+    CharSequence rewrited = FiniteStatePostprocessingMachine.rewrite(
+        "class My extends java.util.Set {}");
+
+    check(rewrited).hasToString(
+        LINES.join("import java.util.Set;",
+            "class My extends Set {}"));
+
+    rewrited = FiniteStatePostprocessingMachine.rewrite(
+        LINES.join(
+            "package start;",
+            "class My extends java.util.Set {}"));
+
+    check(rewrited).hasToString(
+        LINES.join("package start;",
+            "import java.util.Set;",
+            "class My extends Set {}"));
+  }
+
+}
