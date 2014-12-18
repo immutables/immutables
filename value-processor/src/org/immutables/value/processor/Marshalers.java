118cc436dcd46adc6e5a52f7b6bf27679037096d
Eugene Lukash <e.lucash@gmail.com>
2014-12-18 02:51:03 -0800

    json unmarshaling: marginally more performant switch by first char

--- value-processor/src/org/immutables/value/processor/Marshalers.java
+++ value-processor/src/org/immutables/value/processor/Marshalers.java
@@ -15,7 +15,27 @@
  */
 package org.immutables.value.processor;
 
+import com.google.common.base.Function;
+import com.google.common.collect.ImmutableMultimap;
+import com.google.common.collect.Multimap;
 import org.immutables.generator.Generator;
+import org.immutables.value.processor.meta.ValueAttribute;
 
 @Generator.Template
-abstract class Marshalers extends ValuesTemplate {}
+abstract class Marshalers extends ValuesTemplate {
+  @Generator.Typedef
+  Multimap<Character, ValueAttribute> Mm;
+  public final Function<Iterable<ValueAttribute>, Multimap<Character, ValueAttribute>> byFirstCharacter =
+      new Function<Iterable<ValueAttribute>, Multimap<Character, ValueAttribute>>() {
+        @Override
+        public Multimap<Character, ValueAttribute> apply(Iterable<ValueAttribute> attributes) {
+          ImmutableMultimap.Builder<Character, ValueAttribute> builder = ImmutableMultimap.builder();
+          for (ValueAttribute attribute : attributes) {
+            String name = attribute.name();
+            char firstChar = name.charAt(0);
+            builder.put(firstChar, attribute);
+          }
+          return builder.build();
+        }
+      };
+}
