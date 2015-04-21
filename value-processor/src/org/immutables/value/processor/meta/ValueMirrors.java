22fe51b58cdbf39c63131c618d988d3e0b1ca6ef
Eugene Lukash <e.lucash@gmail.com>
2015-04-21 15:48:41 -0700

    fixing javadocs generation for copyof, removing unnecessary javadocs

--- value-processor/src/org/immutables/value/processor/meta/ValueMirrors.java
+++ value-processor/src/org/immutables/value/processor/meta/ValueMirrors.java
@@ -15,7 +15,6 @@
  */
 package org.immutables.value.processor.meta;
 
-import com.google.common.annotations.Beta;
 import org.immutables.mirror.Mirror;
 
 public final class ValueMirrors {
@@ -27,38 +26,14 @@ public final class ValueMirrors {
   @Mirror.Annotation("org.immutables.value.Value.Immutable")
   public @interface Immutable {
 
-    /**
-     * If {@code singleton=true}, generates internal singleton object constructed without any
-     * specified parameters. Default is {@literal false}.
-     */
     boolean singleton() default false;
 
-    /**
-     * If {@code intern=true} then instances will be strong interned on construction.
-     * Default is {@literal false}.
-     * @see com.google.common.collect.Interners#newStrongInterner()
-     */
     boolean intern() default false;
 
-    /**
-     * If {@code copy=false} then generation of copying methods will be disabled.
-     * This appies to static "copyOf" methods as well as modify-by-copy "withAttributeName" methods.
-     * Default is {@literal false}, i.e do not generate copy methods.
-     */
     boolean copy() default false;
 
-    /**
-     * If {@code prehash=true} then {@code hashCode} will be precomputed during construction.
-     * This could speed up collection lookups for objects with lots of attributes and nested
-     * objects.
-     * In general, use this when {@code hashCode} computation is expensive and will be used a lot.
-     */
     boolean prehash() default false;
 
-    /**
-     * If {@code builder=false}, disables generation of {@code builder()}. Default is
-     * {@literal true}.
-     */
     boolean builder() default true;
   }
 
@@ -101,197 +76,57 @@ public final class ValueMirrors {
 
   @Mirror.Annotation("org.immutables.value.Value.Style")
   public @interface Style {
-    /**
-     * Patterns to recognize accessors.
-     * @return naming template
-     */
     String[] get() default {};
 
-    /**
-     * Builder initialization method. i.e. "setter" in builder.
-     * @return naming template
-     */
     String init() default "*";
 
-    /**
-     * Modify-by-copy "with" method.
-     * @return naming template
-     */
     String with() default "with*";
 
-    /**
-     * Add value to collection attribute from iterable
-     * @return naming template
-     */
     String add() default "add*";
 
-    /**
-     * Add all values to collection attribute from iterable
-     * @return naming template
-     */
     String addAll() default "addAll*";
 
-    /**
-     * Puts entry to a map attribute
-     * @return naming template
-     */
     String put() default "put*";
 
-    /**
-     * Puts all entries to a map attribute
-     * @return naming template
-     */
     String putAll() default "putAll*";
 
-    /**
-     * Copy constructor method name.
-     * @return naming template
-     */
     String copyOf() default "copyOf";
 
-    /**
-     * Constructor method name.
-     * @return naming template
-     */
     String of() default "of";
 
-    /**
-     * Singleton accessor method name
-     * @return naming template
-     */
     String instance() default "of";
 
-    /**
-     * Builder creator method. This naming allow special keyword "new" value.
-     * This will customize builder to be created using constructor rather than
-     * factory method.
-     * @return naming template
-     */
     String builder() default "builder";
 
-    /**
-     * Builder creator method, it differes from {@link #builder()} in that this naming is used for
-     * builders that are external to immutable objects, such as top level builders for values or
-     * factories. This naming allow special keyword "new" value, which is the default.
-     * "new" will customize builder to be created using constructor rather than
-     * factory method.
-     * @return naming template
-     */
     String newBuilder() default "new";
 
-    /**
-     * Method to initialize builder with values from instance.
-     * @return naming template
-     */
     String from() default "from";
 
-    /**
-     * Instance creation method on builder.
-     * @return naming template
-     */
     String build() default "build";
 
-    /**
-     * Builder class name.
-     * @return naming template
-     */
     String typeBuilder() default "Builder";
 
-    /**
-     * Naming templates to detect base/raw type name from provided abstract value type name.
-     * @return naming templates
-     */
     String[] typeAbstract() default {};
 
-    /**
-     * Modifiable type name template.
-     * @return naming template
-     */
     String typeImmutable() default "Immutable*";
 
-    /**
-     * Umbrella nesting class name generated using {@link Enclosing}.
-     * @return naming template
-     */
     String typeImmutableEnclosing() default "Immutable*";
 
-    /**
-     * Immutable class name when generated under umbrella class using {@link Enclosing} annotation.
-     * @see #typeImmutable()
-     * @see #typeImmutableEnclosing()
-     * @return naming template
-     */
     String typeImmutableNested() default "*";
 
-    /**
-     * Specify default options for the generated immutable objects.
-     * If at least one attribute is specifid in inline {@literal @}{@link Immutable} annotation,
-     * then this default will not be taken into account, objects will be generated using attributes
-     * from inline annotation.
-     * @return default configuration
-     */
     Immutable defaults() default @Immutable;
 
-    /**
-     * When {@code true} &mdash; forces to generate strict builder code. Strict builders are forward
-     * only. For collections and maps, there's no set/reset methods are generated in
-     * favor of using additive only initializers. For regular attributes, initializers could be
-     * called only once, subsequent reinitialization with throw exception. Usage of strict builders
-     * may help to prevent initialization mistakes.
-     * @return if strict builder enabled
-     */
     boolean strictBuilder() default false;
 
-    /**
-     * Specify the mode in which accibility visibility is derived from abstract value type.
-     * It is a good idea to not specify such attributea inline with immutable values, but rather
-     * create style annotation (@see Style).
-     * @return implementation visibility
-     */
-    @Beta
     ImplementationVisibility visibility() default ImplementationVisibility.SAME;
 
-    /**
-     * When {@code true} &mdash; forces to generate code which use only JDK 7+ standard library
-     * classes. It is {@code false} by default, however usage of JDK-only classes will be turned on
-     * automatically if <em>Google Guava</em> library is not found in classpath. The generated code
-     * will have subtle differences, but nevertheless will be functionally equivalent.
-     * <p>
-     * <em>Note that some additional annotation processors may not work without
-     * Guava being accessible to the generated classes</em>
-     * @return if forced JDK-only class usage
-     */
     boolean jdkOnly() default false;
 
-    /**
-     * If implementation visibility is more restrictive than visibility of abstract value type, then
-     * implementation type will not be exposed as a return type of {@code build()} or {@code of()}
-     * constructon methods. Builder visibility will follow.
-     */
-    @Beta
     public enum ImplementationVisibility {
-      /**
-       * Generated implementation class forced to be public.
-       */
       PUBLIC,
-      /**
-       * Visibility is the same
-       */
       SAME,
-
-      /**
-       * Visibility is the same, but it is not returned from build and factory method, instead
-       * abstract value type returned.
-       */
       SAME_NON_RETURNED,
-      /**
-       * 
-       */
       PACKAGE,
-      /**
-       * Allowed only when builder is enabled or nested inside enclosing type.
-       * Builder visibility will follow the umbrella class visibility.
-       */
       PRIVATE
     }
   }
