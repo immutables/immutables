73e2f1d405a20d70e7b4560ec78e6d0364ba0ecd
Augusto Travillio <augusto.travillio@gmail.com>
2015-05-22 19:00:08 -0700

    Organize imports

--- value-processor/src/org/immutables/value/processor/meta/ValueType.java
+++ value-processor/src/org/immutables/value/processor/meta/ValueType.java
@@ -15,33 +15,30 @@
  */
 package org.immutables.value.processor.meta;
 
-import javax.lang.model.element.ExecutableElement;
-import java.util.Collection;
-import java.util.Map.Entry;
-import com.google.common.collect.ArrayListMultimap;
-import com.google.common.collect.Multimap;
-import com.google.common.collect.ImmutableMultimap;
 import com.google.common.base.CaseFormat;
 import com.google.common.base.Function;
 import com.google.common.base.Optional;
 import com.google.common.base.Predicate;
 import com.google.common.base.Predicates;
+import com.google.common.collect.ArrayListMultimap;
 import com.google.common.collect.FluentIterable;
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableSet;
 import com.google.common.collect.Iterables;
 import com.google.common.collect.Lists;
+import com.google.common.collect.Multimap;
 import com.google.common.collect.Ordering;
 import com.google.common.collect.Sets;
 import java.util.Arrays;
+import java.util.Collection;
 import java.util.Collections;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Map.Entry;
 import java.util.Objects;
 import java.util.Set;
 import javax.annotation.Nullable;
-import javax.lang.model.element.AnnotationMirror;
 import javax.lang.model.element.Element;
 import javax.lang.model.element.ElementKind;
 import javax.lang.model.element.TypeElement;
