04da04c571529dcc4b92e063e8be6b3f081ba597
ldriscoll <ldriscoll@hubspot.com>
2015-09-16 09:37:57 -0400

    ability to specify what exception to throw on build.

--- value-fixture/test/org/immutables/fixture/style/SpecifiedExceptionTest.java
+++ value-fixture/test/org/immutables/fixture/style/SpecifiedExceptionTest.java
@@ -6,7 +6,12 @@ import nonimmutables.SampleRuntimeException;
 
 public class SpecifiedExceptionTest {
   @Test(expected = SampleRuntimeException.class)
-  public void missingAttributesCheck() {
+  public void itThrowsExpectedConfiguredException() {
     ImmutableSpecifiedException.builder().build();
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void itThrowsSpecifiedExceptionOnBuild() {
+    ImmutableSpecifiedException.builder().build(IllegalArgumentException::new);
   }
 }
