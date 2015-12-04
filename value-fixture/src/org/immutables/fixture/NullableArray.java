f0fb757e0d2773cb3e1491accb218083337b6cc7
Eugene Lukash <e.lucash@gmail.com>
2015-12-04 03:44:45 -0800

    #191 correction to Modifiables for nullable arrays

--- value-fixture/src/org/immutables/fixture/NullableArray.java
+++ value-fixture/src/org/immutables/fixture/NullableArray.java
@@ -3,6 +3,14 @@ package org.immutables.fixture;
 import javax.annotation.Nullable;
 import org.immutables.value.Value;
 
+@Value.Modifiable
+@Value.Immutable
+@Value.Style(jdkOnly = true)
+abstract class NullableRef {
+  @Nullable
+  abstract Object[] refs();
+}
+
 @Value.Immutable
 public interface NullableArray {
   @Nullable
