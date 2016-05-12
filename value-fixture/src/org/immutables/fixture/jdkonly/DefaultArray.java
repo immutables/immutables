27aeada9938d5f4ea36ffb8c4253895d86dc6fe7
Eugene Lukash <e.lucash@gmail.com>
2016-05-12 00:31:20 -0700

    adding extra but righteous safety to get default value

--- value-fixture/src/org/immutables/fixture/jdkonly/DefaultArray.java
+++ value-fixture/src/org/immutables/fixture/jdkonly/DefaultArray.java
@@ -1,11 +1,18 @@
 package org.immutables.fixture.jdkonly;
 
+import javax.annotation.Nullable;
 import org.immutables.value.Value;
 
 @Value.Immutable
 public abstract class DefaultArray {
   @Value.Default
   int[] prop() {
+    return new int[0];
+  }
+
+  @Nullable
+  @Value.Default
+  int[] props() {
     return new int[0];
   }
 }
