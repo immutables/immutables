f06c2fb83e17fbbe90686d9bceb6a29393becefc
Eugene Lukash <e.lucash@gmail.com>
2016-06-02 23:49:47 -0700

    #348 use relativeRaw instead of relative for quialified validate

---
+++ value-fixture/src/org/immutables/fixture/generics/AbstractValidate.java
@@ -0,0 +1,34 @@
+/*
+   Copyright 2016 Immutables Authors and Contributors
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+ */
+package org.immutables.fixture.generics;
+
+import org.immutables.value.Value;
+
+// Compilation test for qualified `validate` call during construction
+interface AbstractValidate<T> {
+
+  @Value.Immutable(intern = true)
+  static abstract class Concrete1<T> implements AbstractValidate<T> {
+    @Value.Parameter
+    abstract T reference();
+  }
+
+  @Value.Immutable(intern = true)
+  static abstract class Concrete2<T> implements AbstractValidate<T> {
+    @Value.Parameter
+    abstract T reference();
+  }
+}
