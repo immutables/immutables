e1bb8aa88863d599c4b2624b140f6e71e1906bfe
asereda-gs <andrei+github@sereda.cc>
2017-05-24 23:46:55 -0400

    addressed PR comments

--- mongo/test/org/immutables/mongo/fixture/BasicMongoOperationsTest.java
+++ mongo/test/org/immutables/mongo/fixture/BasicMongoOperationsTest.java
@@ -1,7 +1,6 @@
 package org.immutables.mongo.fixture;
 
 import org.immutables.mongo.types.Binary;
-import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 
@@ -14,51 +13,46 @@ import static org.immutables.check.Checkers.check;
  */
 public class BasicMongoOperationsTest {
 
-    @Rule
-    public final MongoContext context = new MongoContext();
+  @Rule
+  public final MongoContext context = new MongoContext();
 
-    private ItemRepository repository;
+  private final ItemRepository repository = new ItemRepository(context.setup());
 
-    @Before
-    public void setUp() throws Exception {
-        repository = new ItemRepository(context.setup());
-    }
+  @Test
+  public void empty() throws Exception {
+    check(repository.findAll().fetchAll().getUnchecked()).isEmpty();
+    check(repository.findAll().fetchFirst().getUnchecked()).isAbsent();
+    check(repository.findById("").fetchAll().getUnchecked()).isEmpty();
+    check(repository.findById("MISSING").fetchAll().getUnchecked()).isEmpty();
+  }
 
-    @Test
-    public void empty() throws Exception {
-        check(repository.findAll().fetchAll().getUnchecked()).isEmpty();
-        check(repository.findAll().fetchFirst().getUnchecked()).isAbsent();
-        check(repository.findById("").fetchAll().getUnchecked()).isEmpty();
-        check(repository.findById("MISSING").fetchAll().getUnchecked()).isEmpty();
-    }
+  @Test
+  public void readWrite_single() throws Exception {
+    Item item = ImmutableItem.builder()
+            .id("1")
+            .binary(Binary.create(new byte[]{1, 2, 3}))
+            .build();
 
-    @Test
-    public void readWrite_single() throws Exception {
-        Item item = ImmutableItem.builder()
-                .id("1")
-                .binary(Binary.create(new byte[] {1, 2, 3}))
-                .build();
+    check(repository.upsert(item).getUnchecked()).is(1);
 
-        check(repository.upsert(item).getUnchecked()).is(1);
+    List<Item> items = repository.find(repository.criteria().id("1")).fetchAll().getUnchecked();
 
-        List<Item> items = repository.find(repository.criteria().id("1")).fetchAll().getUnchecked();
+    check(items).hasSize(1);
+    check(items.get(0).id()).is("1");
+    check(repository.findById("1").fetchAll().getUnchecked()).hasSize(1);
+  }
 
-        check(items).hasSize(1);
-        check(items.get(0).id()).is("1");
-        check(repository.findById("1").fetchAll().getUnchecked()).hasSize(1);
-    }
+  @Test
+  public void delete() throws Exception {
+    Item item = ImmutableItem.builder()
+            .id("1")
+            .binary(Binary.create(new byte[0]))
+            .build();
 
-    @Test
-    public void delete() throws Exception {
-        Item item = ImmutableItem.builder()
-                .id("1")
-                .binary(Binary.create(new byte[0]))
-                .build();
-
-        check(repository.insert(item).getUnchecked()).is(1);
-        // expect single entry to be deleted
-        check(repository.findAll().deleteAll().getUnchecked()).is(1);
-        // second time no entries remaining
-        check(repository.findAll().deleteAll().getUnchecked()).is(0);
-    }
+    check(repository.insert(item).getUnchecked()).is(1);
+    // expect single entry to be deleted
+    check(repository.findAll().deleteAll().getUnchecked()).is(1);
+    // second time no entries remaining
+    check(repository.findAll().deleteAll().getUnchecked()).is(0);
+  }
 }
