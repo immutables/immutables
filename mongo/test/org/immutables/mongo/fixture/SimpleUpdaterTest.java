80e41c0c530f210ee2dc94919b877e327ee2ba40
asereda-gs <andrei.sereda@gs.com>
2017-05-25 01:30:17 -0400

    codestyle fixes from previous PR (#625)
    * added $set operator for sub-arrays of a document (to override the whole array)
    
    * removed unnecessary test
    
    * addressed PR comments

--- mongo/test/org/immutables/mongo/fixture/SimpleUpdaterTest.java
+++ mongo/test/org/immutables/mongo/fixture/SimpleUpdaterTest.java
@@ -2,7 +2,6 @@ package org.immutables.mongo.fixture;
 
 import org.bson.types.ObjectId;
 import org.immutables.mongo.types.Id;
-import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 
@@ -15,139 +14,134 @@ import static org.immutables.check.Checkers.check;
 
 public class SimpleUpdaterTest {
 
-    @Rule
-    public final MongoContext context = new MongoContext();
+  @Rule
+  public final MongoContext context = new MongoContext();
 
-    private ItemRepository repository;
+  private final ItemRepository repository = new ItemRepository(context.setup());
 
-    @Before
-    public void setUp() throws Exception {
-        this.repository = new ItemRepository(context.setup());
-    }
+  /**
+   * Checks {@code $set} functionality of the repository
+   */
+  @Test
+  public void setList() throws Exception {
+    final String id = "i1";
 
-    /**
-     * Checks {@code $set} functionality of the repository
-     */
-    @Test
-    public void setList() throws Exception {
-        final String id = "i1";
+    check(overrideList(id, Collections.singleton("BAD"))).is(0);
 
-        check(overrideList(id, Collections.singleton("BAD"))).is(0);
+    ImmutableItem item = ImmutableItem.builder().id(id).addList("l1").build();
 
-        ImmutableItem item = ImmutableItem.builder().id(id).addList("l1").build();
+    check(repository.upsert(item).getUnchecked()).is(1);
+    check(findById(id).list()).hasAll("l1");
 
-        check(repository.upsert(item).getUnchecked()).is(1);
-        check(findById(id).list()).hasAll("l1");
+    overrideList(id, Collections.<String>emptyList());
+    check(findById(id).list()).isEmpty();
 
-        overrideList(id, Collections.<String>emptyList());
-        check(findById(id).list()).isEmpty();
+    overrideList(id, Collections.singleton("l2"));
+    check(findById(id).list()).hasAll("l2");
 
-        overrideList(id, Collections.singleton("l2"));
-        check(findById(id).list()).hasAll("l2");
+    overrideList(id, Arrays.asList("l3", "l4", "l5"));
+    check(findById(id).list()).hasAll("l3", "l4", "l5");
+  }
 
-        overrideList(id, Arrays.asList("l3", "l4", "l5"));
-        check(findById(id).list()).hasAll("l3", "l4", "l5");
-    }
+  @Test
+  public void set_with_other_operations() throws Exception {
+    final String id = "i1";
 
-    @Test
-    public void set_with_other_operations() throws Exception {
-        final String id = "i1";
+    ImmutableItem item = ImmutableItem.builder().id(id).addList("l1").build();
 
-        ImmutableItem item = ImmutableItem.builder().id(id).addList("l1").build();
+    check(repository.upsert(item).getUnchecked()).is(1);
 
-        check(repository.upsert(item).getUnchecked()).is(1);
+    repository.update(repository.criteria().id(id))
+            .setTags(Collections.singleton(ImmutableTag.of("t1")))
+            .setList(Collections.singleton("l1"))
+            .setIds(Collections.singleton(Id.fromString(new ObjectId().toString())))
+            .updateFirst()
+            .getUnchecked();
 
-        repository.update(repository.criteria().id(id))
-                .setTags(Collections.singleton(ImmutableTag.of("t1")))
-                .setList(Collections.singleton("l1"))
-                .setIds(Collections.singleton(Id.fromString(new ObjectId().toString())))
-                .updateFirst()
-                .getUnchecked();
+    final Item item2 = findById(id);
 
-        final Item item2 = findById(id);
+    check(item2.list()).hasAll("l1");
+    check(item2.tags()).hasAll(ImmutableTag.of("t1"));
+    check(item2.ids()).hasSize(1);
+  }
 
-        check(item2.list()).hasAll("l1");
-        check(item2.tags()).hasAll(ImmutableTag.of("t1"));
-        check(item2.ids()).hasSize(1);
-    }
+  /**
+   * {@code $set} functionality but on a non-scalar Object
+   */
+  @Test
+  public void setList_non_scalar_object() throws Exception {
+    final String id = "i1";
+    ImmutableItem item = ImmutableItem.builder().id(id).addList("l1").addTags(ImmutableTag.of("t1")).build();
 
-    /**
-     * {@code $set} functionality but on a non-scalar Object
-     */
-    @Test
-    public void setList_non_scalar_object() throws Exception {
-        final String id = "i1";
-        ImmutableItem item = ImmutableItem.builder().id(id).addList("l1").addTags(ImmutableTag.of("t1")).build();
+    check(repository.upsert(item).getUnchecked()).is(1);
 
-        check(repository.upsert(item).getUnchecked()).is(1);
+    repository.update(repository.criteria().id(id)).setTags(Collections.<Item.Tag>emptyList()).updateFirst().getUnchecked();
 
-        repository.update(repository.criteria().id(id)).setTags(Collections.<Item.Tag>emptyList()).updateFirst().getUnchecked();
+    check(findById(id).tags()).isEmpty();
 
-        check(findById(id).tags()).isEmpty();
+    final Set<? extends Item.Tag> set1 = Collections.singleton(ImmutableTag.of("t2"));
+    repository.update(repository.criteria().id(id))
+            .setTags(set1)
+            .updateFirst()
+            .getUnchecked();
 
-        final Set<? extends Item.Tag> set1 = Collections.singleton(ImmutableTag.of("t2"));
-        repository.update(repository.criteria().id(id))
-                .setTags(set1)
-                .updateFirst()
-                .getUnchecked();
-
-        check(findById(id).tags()).hasAll(set1);
+    check(findById(id).tags()).hasAll(set1);
 
 
-        final List<? extends Item.Tag> set2 = Arrays.asList(ImmutableTag.of("t3"), ImmutableTag.of("t4"));
+    final List<? extends Item.Tag> set2 = Arrays.asList(ImmutableTag.of("t3"), ImmutableTag.of("t4"));
 
-        repository.update(repository.criteria().id(id))
-                .setTags(set2)
-                .updateFirst()
-                .getUnchecked();
+    repository.update(repository.criteria().id(id))
+            .setTags(set2)
+            .updateFirst()
+            .getUnchecked();
 
-        check(findById(id).tags()).hasAll(set2);
-    }
+    check(findById(id).tags()).hasAll(set2);
+  }
 
-    /**
-     * Checks {@code $push} functionality of repository
-     */
-    @Test
-    public void addToList() throws Exception {
-        final String id = "i1";
-        ImmutableItem item = ImmutableItem.builder().id(id).addList("l1").build();
+  /**
+   * Checks {@code $push} functionality of repository
+   */
+  @Test
+  public void addToList() throws Exception {
+    final String id = "i1";
+    ImmutableItem item = ImmutableItem.builder().id(id).addList("l1").build();
 
-        check(repository.upsert(item).getUnchecked()).is(1);
+    check(repository.upsert(item).getUnchecked()).is(1);
 
-        push(id, Collections.singleton("l2"));
-        check(findById(id).list()).hasAll("l1", "l2");
+    push(id, Collections.singleton("l2"));
+    check(findById(id).list()).hasAll("l1", "l2");
 
-        push(id, Arrays.asList("l3", "l4"));
-        check(findById(id).list()).hasAll("l1", "l2", "l3", "l4");
-    }
+    push(id, Arrays.asList("l3", "l4"));
+    check(findById(id).list()).hasAll("l1", "l2", "l3", "l4");
+  }
 
-    @Test
-    public void clear() throws Exception {
-        final String id = "i1";
-        ImmutableItem item = ImmutableItem.builder().id(id).addList("l1").build();
-        repository.upsert(item).getUnchecked();
+  @Test
+  public void clear() throws Exception {
+    final String id = "i1";
+    ImmutableItem item = ImmutableItem.builder().id(id).addList("l1").build();
+    repository.upsert(item).getUnchecked();
 
-        repository.update(repository.criteria().id(id))
-                .clearList().updateFirst().getUnchecked();
+    repository.update(repository.criteria().id(id))
+            .clearList().updateFirst().getUnchecked();
 
-        check(findById(id).list()).isEmpty();
-    }
+    check(findById(id).list()).isEmpty();
+  }
 
-    private int push(String id, Iterable<String> values) {
-        return repository.update(repository.criteria().id(id))
-                .addAllList(values)
-                .updateFirst()
-                .getUnchecked();
-    }
+  private int push(String id, Iterable<String> values) {
+    return repository.update(repository.criteria().id(id))
+            .addAllList(values)
+            .updateFirst()
+            .getUnchecked();
+  }
 
-    private int overrideList(String id, Iterable<String> list) {
-        return repository.update(repository.criteria().id(id))
-                .setList(list)
-                .updateFirst()
-                .getUnchecked();
-    }
+  private int overrideList(String id, Iterable<String> list) {
+    return repository.update(repository.criteria().id(id))
+            .setList(list)
+            .updateFirst()
+            .getUnchecked();
+  }
 
-    private Item findById(String id) {
-        return repository.findById(id).fetchFirst().getUnchecked().get();
-    }
+  private Item findById(String id) {
+    return repository.findById(id).fetchFirst().getUnchecked().get();
+  }
 }
