396e094b7cf5b3b695d4ed4a457adbae8f615236
Andrei Sereda <25229979+asereda-gs@users.noreply.github.com>
2019-06-06 13:01:17 -0400

    Criteria. Introduce Service interface as internal API
    Move Publishers class into internal package

--- criteria/common/src/org/immutables/criteria/internal/Reactive.java
+++ criteria/common/src/org/immutables/criteria/internal/Reactive.java
@@ -1,4 +1,20 @@
-package org.immutables.criteria.elasticsearch;
+/*
+ * Copyright 2019 Immutables Authors and Contributors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.immutables.criteria.internal;
 
 import org.reactivestreams.Publisher;
 import org.reactivestreams.Subscriber;
@@ -10,31 +26,41 @@ import java.util.function.Function;
 import java.util.function.Supplier;
 
 /**
- * Utils for reactive streams
+ * Utils for <a href="https://www.reactive-streams.org/">reactive streams</a> with typical operators
+ * like map / error / flatMap / flatMapIterable which are applied on a {@link Publisher}.
+ *
+ * <p>This is internal class and should not be used outside criteria and adapters.
+ *
+ * <p>It is currently a naive implementation which didn't pass the
+ * <a href="https://github.com/reactive-streams/reactive-streams-jvm/tree/master/tck">TCK</a>
+ *
+ * <p>Wherever Criteria should depend directly on existing reactive libraries
+ * like <a href="https://github.com/ReactiveX/RxJava">rxjava</a> or <a href="https://projectreactor.io/">reactor</a>
+ * instead of home-made implementation is a valid question and will be decided later.
  */
-final class Publishers {
+public final class Reactive {
 
-  private Publishers() {}
+  private Reactive() {}
 
   /**
    *  Converts streaming elements into a different type using provided {@code mapper}
    */
-  static <T, U> Publisher<U> map(Publisher<T> source, Function<? super T, ? extends U> mapper) {
+  public static <T, U> Publisher<U> map(Publisher<T> source, Function<? super T, ? extends U> mapper) {
     return new MapPublisher<>(mapper, source);
   }
 
-  static <T> Publisher<T> error(Supplier<? extends Throwable> errorSupplier) {
+  public static <T> Publisher<T> error(Supplier<? extends Throwable> errorSupplier) {
     return new ErrorPublisher<>(errorSupplier);
   }
 
   /**
    * Used to convert {@code Publisher<Iterable<U>>} into {@code Publisher<U>}
    */
-  static  <T, U> Publisher<U> flatMapIterable(Publisher<T> publisher, final Function<? super T, ? extends Iterable<? extends U>> mapper) {
+  public static  <T, U> Publisher<U> flatMapIterable(Publisher<T> publisher, final Function<? super T, ? extends Iterable<? extends U>> mapper) {
       return new FlattenIterablePublisher<>(publisher, mapper);
   }
 
-  static <T> Publisher<T> error(Throwable e) {
+  public static <T> Publisher<T> error(Throwable e) {
     return error(() -> e);
   }
 
