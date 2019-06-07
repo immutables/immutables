42114b9b3942b2049034e4f25bb2e7f360ce4835
Andrei Sereda <25229979+asereda-gs@users.noreply.github.com>
2019-06-07 13:47:14 -0400

    Add PersonGenerator class to simplify test object creation
    Only set fields you're interested in and skip required ones

--- criteria/common/test/org/immutables/criteria/personmodel/PersonGenerator.java
+++ criteria/common/test/org/immutables/criteria/personmodel/PersonGenerator.java
@@ -16,25 +16,95 @@
 
 package org.immutables.criteria.personmodel;
 
+import com.google.common.base.Preconditions;
+import com.google.common.collect.ImmutableList;
+
+import java.time.Clock;
 import java.time.LocalDate;
-import java.util.Objects;
+import java.util.Iterator;
+import java.util.concurrent.atomic.AtomicLong;
+import java.util.stream.Stream;
+import java.util.stream.StreamSupport;
 
-public final class PersonGenerator {
+/**
+ * Generates {@link Person} objects with some pre-populated data. To be used for tests
+ * to speedup entity creation (avoid providing all required fields).
+ */
+public final class PersonGenerator implements Iterable<ImmutablePerson> {
 
-  private PersonGenerator() {}
+  private final static ImmutableList<String> FIRST_NAMES = ImmutableList.of("John", "James",
+          "Robert", "Jennifer", "Michael", "Linda", "William", "Elizabeth", "Lucas", "Lea");
 
-  /**
-   * Creates a simple person directly from fullName
-   */
-  public static Person of(String fullName) {
-    Objects.requireNonNull(fullName, "fullName");
-    return ImmutablePerson.builder()
-            .fullName(fullName)
-            .id(Integer.toHexString(fullName.hashCode()))
-            .dateOfBirth(LocalDate.of(1997, 4, 15))
-            .isActive(true)
-            .age(22)
-            .build();
+  private final static ImmutableList<String> LAST_NAMES = ImmutableList.of("Smith", "Green",
+          "Brown", "Lopez", "Rodriguez", "Miller", "Cox", "Taylor", "Petit", "Leroy");
+
+  private final static ImmutableList<String> NICKNAMES = ImmutableList.of("007", "bitmap",
+          "flake", "superman", "blade", "clea", "calypso", "slyde", "hulk");
+  
+  private final AtomicLong index;
+  private final Clock clock;
+
+  public PersonGenerator() {
+    this(Clock.systemDefaultZone());
   }
 
+  public PersonGenerator(Clock clock) {
+    Preconditions.checkState(FIRST_NAMES.size() == LAST_NAMES.size(), "%s != %s",
+            FIRST_NAMES.size(), LAST_NAMES.size());
+    this.index = new AtomicLong();
+    this.clock = clock;
+  }
+
+  public ImmutablePerson next() {
+    return get((int) index.getAndIncrement() % FIRST_NAMES.size());
+  }
+
+  /**
+   * Generate person at particular "index"
+   */
+  private ImmutablePerson get(int index) {
+    Preconditions.checkArgument(index < FIRST_NAMES.size(),
+            "expected index %s < %s", index, FIRST_NAMES.size());
+
+    // add variation to ages so it doesn't look too consecutive 20, 21, 22 etc.
+    final int age = 20 + (index * 37 >> 4) % 30;
+
+    final ImmutablePerson.Builder builder = ImmutablePerson.builder()
+                .fullName(FIRST_NAMES.get(index) + " " + LAST_NAMES.get(index))
+                .id("id" + index)
+                .dateOfBirth(LocalDate.now(clock).minusYears(age))
+                .isActive(index % 2 == 0)
+                .age(age)
+                .nickName(NICKNAMES.get(index % NICKNAMES.size()));
+
+    return builder.build();
+  }
+
+  /**
+   * Unbounded stream over generated entries
+   */
+  public Stream<ImmutablePerson> stream() {
+    return StreamSupport.stream(spliterator(), false);
+  }
+
+  /**
+   * Returns an infinite iterator over generated entries. You're advised to use a limiting function
+   * like {@code Iterators.limit(...)}.
+   *
+   * @return unbounded iterator with valid entries
+   */
+  @Override
+  public Iterator<ImmutablePerson> iterator() {
+    return new Iterator<ImmutablePerson>() {
+      @Override
+      public boolean hasNext() {
+        return false;
+      }
+
+      @Override
+      public ImmutablePerson next() {
+        return PersonGenerator.this.next();
+      }
+    };
+  }
 }
