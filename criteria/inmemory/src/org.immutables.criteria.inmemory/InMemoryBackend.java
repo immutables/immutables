9d398762c736955581a78f45e4f29b8bf2910b19
Andrei Sereda <25229979+asereda-gs@users.noreply.github.com>
2019-06-28 18:42:26 -0400

    Implement InMemoryBackend

---
+++ criteria/inmemory/src/org.immutables.criteria.inmemory/InMemoryBackend.java
@@ -0,0 +1,100 @@
+/*
+ * Copyright 2019 Immutables Authors and Contributors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.immutables.criteria.inmemory;
+
+import io.reactivex.Flowable;
+import org.immutables.criteria.Criteria;
+import org.immutables.criteria.WriteResult;
+import org.immutables.criteria.adapter.Backend;
+import org.immutables.criteria.adapter.Operations;
+import org.immutables.criteria.adapter.UnknownWriteResult;
+import org.immutables.criteria.expression.Query;
+import org.reactivestreams.Publisher;
+
+import java.util.Map;
+import java.util.Objects;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+/**
+ * Backend backed by a {@link Map} (usually {@link java.util.concurrent.ConcurrentMap}).
+ */
+public class InMemoryBackend implements Backend {
+
+  private final Map<Object, Object> store;
+
+  public InMemoryBackend() {
+    this(new ConcurrentHashMap<>());
+  }
+
+  public InMemoryBackend(Map<Object, Object> store) {
+    this.store = Objects.requireNonNull(store, "store");
+  }
+
+  @Override
+  public <T> Publisher<T> execute(Operation<T> operation) {
+    if (operation instanceof Operations.Select) {
+      return query((Operations.Select<T>) operation);
+    } else if (operation instanceof Operations.Insert) {
+      return (Publisher<T>) insert((Operations.Insert<T>) operation);
+    }
+
+    return Flowable.error(new UnsupportedOperationException(String.format("Operation %s not supported", operation)));
+  }
+
+  private <T> Publisher<T> query(Operations.Select<T> select) {
+    final Query query = select.query();
+    Stream<T> stream = (Stream<T>) store.values().stream();
+    if (query.filter().isPresent()) {
+      Predicate<T> predicate = InMemoryExpressionEvaluator.of(query.filter().get());
+
+      stream = stream.filter(predicate);
+    }
+
+    if (query.offset().isPresent()) {
+      stream = stream.skip(query.offset().getAsLong());
+    }
+
+    if (query.limit().isPresent()) {
+      stream = stream.limit(query.limit().getAsLong());
+    }
+
+    return Flowable.fromIterable(stream.collect(Collectors.toList()));
+  }
+
+  private <T> Publisher<WriteResult> insert(Operations.Insert<T> op) {
+    if (!(op instanceof Operations.KeyedInsert)) {
+      throw new UnsupportedOperationException(
+              String.format("%s supports only %s. Did you define a key (@%s) on your domain class ?",
+                      InMemoryBackend.class.getSimpleName(),
+                      Operations.KeyedInsert.class.getSimpleName(),
+                      Criteria.Id.class.getName()));
+    }
+
+    final Operations.KeyedInsert<?, T> insert = (Operations.KeyedInsert<?, T>) op;
+    final Map<Object, T> store = (Map<Object, T>) this.store;
+    return Flowable.fromCallable(() -> {
+      store.putAll(insert.toMap());
+      return UnknownWriteResult.INSTANCE;
+    });
+
+  }
+
+
+}
---
+++ criteria/inmemory/test/org/immutables/criteria/inmemory/InMemoryPersonTest.java
@@ -0,0 +1,44 @@
+/*
+ * Copyright 2019 Immutables Authors and Contributors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.immutables.criteria.inmemory;
+
+import org.immutables.criteria.personmodel.AbstractPersonTest;
+import org.immutables.criteria.personmodel.PersonRepository;
+import org.junit.Before;
+
+import java.util.EnumSet;
+import java.util.Set;
+
+public class InMemoryPersonTest extends AbstractPersonTest  {
+
+  private final PersonRepository repository = new PersonRepository(new InMemoryBackend());
+
+  @Before
+  public void setUp() throws Exception {
+    populate();
+  }
+
+  @Override
+  protected Set<Feature> features() {
+    return EnumSet.of(Feature.DELETE, Feature.QUERY, Feature.QUERY_WITH_LIMIT, Feature.QUERY_WITH_OFFSET);
+  }
+
+  @Override
+  protected PersonRepository repository() {
+    return repository;
+  }
+}
