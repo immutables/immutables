c7bbd001f7705d30cef3bc3745d2893b832e67bb
Andrei Sereda <25229979+asereda-gs@users.noreply.github.com>
2019-08-01 20:08:39 -0400

    add test for different types of repositories

---
+++ criteria/common/test/org/immutables/criteria/repository/DifferentRepositoriesTest.java
@@ -0,0 +1,50 @@
+/*
+ * Copyright 2019 Immutables Authors and Contributors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.immutables.criteria.repository;
+
+import org.immutables.criteria.adapter.Backend;
+import org.junit.Test;
+import org.reactivestreams.Publisher;
+
+public class DifferentRepositoriesTest {
+
+  @Test
+  public void empty() {
+    EmptyRepository empty = new EmptyRepository(new NoopBackend());
+  }
+
+  @Test
+  public void sync() {
+    SyncModelRepository sync = new SyncModelRepository(new NoopBackend());
+  }
+
+  @Test
+  public void heterogenious() {
+    HeterogeniousRepository repo = new HeterogeniousRepository(new NoopBackend());
+  }
+
+  /**
+   * {@code null} object backend
+   */
+  private static class NoopBackend implements Backend {
+
+    @Override
+    public <T> Publisher<T> execute(Operation operation) {
+      throw new UnsupportedOperationException();
+    }
+  }
+}
---
+++ criteria/common/test/org/immutables/criteria/repository/SyncModel.java
@@ -0,0 +1,29 @@
+/*
+ * Copyright 2019 Immutables Authors and Contributors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.immutables.criteria.repository;
+
+import org.immutables.criteria.Criteria;
+import org.immutables.criteria.repository.sync.SyncReadable;
+import org.immutables.criteria.repository.sync.SyncWritable;
+import org.immutables.value.Value;
+
+@Value.Immutable
+@Criteria
+@Criteria.Repository(facets = {SyncReadable.class, SyncWritable.class})
+interface SyncModel {
+    String foo();
+}
