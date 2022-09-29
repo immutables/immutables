a06561c1a39251607108ee7a3b6d1daf4aeda4c1
Javad Moghisi <j.moghisi@gmail.com>
2022-09-29 21:55:49 +0100

    add test coverage for GsonCodecs

--- mongo/src/org/immutables/mongo/repository/RepositorySetup.java
+++ mongo/src/org/immutables/mongo/repository/RepositorySetup.java
@@ -24,7 +24,6 @@ import com.google.gson.GsonBuilder;
 import com.google.gson.TypeAdapterFactory;
 import com.mongodb.DB;
 import com.mongodb.MongoClient;
-import com.mongodb.MongoClientSettings;
 import com.mongodb.MongoClientURI;
 import com.mongodb.ReadPreference;
 import com.mongodb.WriteConcern;
@@ -220,22 +219,7 @@ public final class RepositorySetup {
     public Builder gson(Gson gson) {
       checkNotNull(gson, "gson");
 
-      // Will be used as a factory for BSON types (if Gson does not have one). By default, uses
-      // TypeAdapter(s) from Gson if they're explicitly defined (not a ReflectiveTypeAdapter).
-      // Otherwise delegate to BSON codec.
-      TypeAdapterFactory bsonAdapterFactory = GsonCodecs.delegatingTypeAdapterFactory(
-              MongoClientSettings.getDefaultCodecRegistry()
-      );
-
-      // Appending new TypeAdapterFactory to allow Gson and Bson adapters to co-exists.
-      // Depending on the type we may need to use one or another. For instance,
-      // Date should be serialized by Gson (even if Bson has codec for it).
-      // But ObjectId / Decimal128 by BSON (if Gson doesn't have a type adapter for it).
-      // Document or BsonDocument should only be handled by BSON (it's unlikely that users have direct dependency on them in POJOs).
-      // So newGson is a way to extend existing Gson instance with "BSON TypeAdapter(s)"
-      Gson newGson = gson.newBuilder()
-              .registerTypeAdapterFactory(bsonAdapterFactory)
-              .create();
+      Gson newGson = GsonCodecs.newGsonWithBsonSupport(gson);
 
       // expose new Gson as CodecRegistry. Using fromRegistries() for caching
       CodecRegistry codecRegistry = CodecRegistries.fromRegistries(GsonCodecs.codecRegistryFromGson(newGson));
