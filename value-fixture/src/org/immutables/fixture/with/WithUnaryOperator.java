d3f772ebc3d5863a7a79cc94336f5e7fd6e06dd5
Eugene Lukash <e.lucash@gmail.com>
2022-10-12 16:44:25 -0400

    tabs etc

--- value-fixture/src/org/immutables/fixture/with/WithUnaryOperator.java
+++ value-fixture/src/org/immutables/fixture/with/WithUnaryOperator.java
@@ -9,11 +9,11 @@ import org.immutables.value.Value;
 @Value.Immutable
 @Value.Style(withUnaryOperator = "map*")
 interface WithUnaryOperator {
-	int a();
-	String b();
-	Optional<String> o();
-	List<String> l();
-	OptionalInt i();
-	OptionalDouble d();
-	com.google.common.base.Optional<Integer> oi();
+  int a();
+  String b();
+  Optional<String> o();
+  List<String> l();
+  OptionalInt i();
+  OptionalDouble d();
+  com.google.common.base.Optional<Integer> oi();
 }
