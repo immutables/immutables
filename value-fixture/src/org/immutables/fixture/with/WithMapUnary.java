99419519130bab84e77ca1cbd67204e999dcaa56
Eugene Lukash <e.lucash@gmail.com>
2022-10-13 12:45:10 -0400

    tabs strikes back

--- value-fixture/src/org/immutables/fixture/with/WithMapUnary.java
+++ value-fixture/src/org/immutables/fixture/with/WithMapUnary.java
@@ -5,16 +5,16 @@ import org.immutables.value.Value;
 @Value.Immutable
 @Value.Style(withUnaryOperator = "map*")
 public interface WithMapUnary extends WithUnaryOperator, WithWithMapUnary {
-	// inherit all attributes from WithUnaryOperator and generate With-interface
-	class Builder extends ImmutableWithMapUnary.Builder {}
+  // inherit all attributes from WithUnaryOperator and generate With-interface
+  class Builder extends ImmutableWithMapUnary.Builder {}
 
-	default void compiledAndUsable() {
-		WithMapUnary u = new ImmutableWithMapUnary.Builder()
-				.a(1)
-				.b("B")
-				.build();
+  default void compiledAndUsable() {
+    WithMapUnary u = new ImmutableWithMapUnary.Builder()
+        .a(1)
+        .b("B")
+        .build();
 
-		// signatures exist after code generation and compilation
-		u = u.mapA(a -> a + 2).mapL(l -> l + "!!!").mapO(o -> o + "???");
-	}
+    // signatures exist after code generation and compilation
+    u = u.mapA(a -> a + 2).mapL(l -> l + "!!!").mapO(o -> o + "???");
+  }
 }
