d294b2d1e28772e063702c4c8df37fff4a25e2c5
elucash <e.lucash@gmail.com>
2025-09-22 19:32:18 -0400

    #1597 just formatting

--- value-fixture/src-java-17/org/immutables/fixture/j17/RecordDefaults.java
+++ value-fixture/src-java-17/org/immutables/fixture/j17/RecordDefaults.java
@@ -5,9 +5,9 @@ import static org.immutables.value.Value.Default;
 
 @Builder
 public record RecordDefaults(
-		@Default.Int(42) int a,
-		@Default.String("ABC") String b,
-		@Default.Long(100) long l,
-		@Default.Long(-100) Long ll,
+    @Default.Int(42) int a,
+    @Default.String("ABC") String b,
+    @Default.Long(100) long l,
+    @Default.Long(-100) Long ll,
     @Default.Double(1.5) double d,
-		String required) {}
+    String required) {}
